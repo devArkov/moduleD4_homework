18e26547eca7278d08eadb3e2c88f85b38d6b815
Alexandr Korobov <devarko@sapo.pt>
2022-12-26 20:34:00 +0300

    Single post comments

--- news/filters.py
+++ news/filters.py
@@ -1,9 +1,14 @@
 from django_filters import FilterSet
-
 from .models import Post
 
 
 class PostFilter(FilterSet):
     class Meta:
         model = Post
-        fields = ('title', 'text', 'type', 'author', 'created_at')
+        fields = {
+            'title': ['icontains'],
+            'text': ['icontains'],
+            'created_at': ['gt'],
+            'author': []
+        }
+
